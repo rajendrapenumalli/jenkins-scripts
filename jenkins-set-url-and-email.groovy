ccd6dafd8080e6347bfe8caa4f8704be8fde9724
Rajendra Prasad Reddy Penumalli <rajendra.penumalli@gmail.com>
2018-11-17 10:46:13 +0530

    Create jenkins-set-url-and-email.groovy

---
+++ jenkins-set-url-and-email.groovy
@@ -0,0 +1,21 @@
+// imports
+import jenkins.model.Jenkins
+import jenkins.model.JenkinsLocationConfiguration
+
+// parameters
+def jenkinsParameters = [
+  email:  'Jenkins Admin <admin@jenkins.com>',
+  url:    'https://ci.jenkins.com:8083/'
+]
+
+// get Jenkins location configuration
+def jenkinsLocationConfiguration = JenkinsLocationConfiguration.get()
+
+// set Jenkins URL
+jenkinsLocationConfiguration.setUrl(jenkinsParameters.url)
+
+// set Jenkins admin email address
+jenkinsLocationConfiguration.setAdminAddress(jenkinsParameters.email)
+
+// save current Jenkins state to disk
+jenkinsLocationConfiguration.save()
