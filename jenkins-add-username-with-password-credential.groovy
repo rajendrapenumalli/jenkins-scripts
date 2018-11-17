702542a38954de0a15182e7e8fc4b1b900eaa3bc
Rajendra Prasad Reddy Penumalli <rajendra.penumalli@gmail.com>
2018-11-17 10:43:30 +0530

    Create jenkins-add-username-with-password-credential.groovy

---
+++ jenkins-add-username-with-password-credential.groovy
@@ -0,0 +1,38 @@
+// imports
+import com.cloudbees.plugins.credentials.*
+import com.cloudbees.plugins.credentials.domains.Domain
+import com.cloudbees.plugins.credentials.impl.*
+import hudson.util.Secret
+import jenkins.model.Jenkins
+
+// parameters
+def jenkinsKeyUsernameWithPasswordParameters = [
+  description:  'Description here',
+  id:           'key-id-here',
+  secret:       '12345678901234567890',
+  userName:     'your-username-here'
+]
+
+// get Jenkins instance
+Jenkins jenkins = Jenkins.getInstance()
+
+// get credentials domain
+def domain = Domain.global()
+
+// get credentials store
+def store = jenkins.getExtensionList('com.cloudbees.plugins.credentials.SystemCredentialsProvider')[0].getStore()
+
+// define Bitbucket secret
+def jenkinsKeyUsernameWithPassword = new UsernamePasswordCredentialsImpl(
+  CredentialsScope.GLOBAL,
+  jenkinsKeyUsernameWithPasswordParameters.id,
+  jenkinsKeyUsernameWithPasswordParameters.description,
+  jenkinsKeyUsernameWithPasswordParameters.userName,
+  jenkinsKeyUsernameWithPasswordParameters.secret
+)
+
+// add credential to store
+store.addCredentials(domain, jenkinsKeyUsernameWithPassword)
+
+// save to disk
+jenkins.save()
